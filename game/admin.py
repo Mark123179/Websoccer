79c73d1c8271d87ed59f136c49563ba42a6e57cc
Mark <markpampa@gmx.de>
2026-05-09 19:50:29 +0200

    Keywörd

--- game/admin.py
+++ game/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import League, Club, Player
 
-
+// KEywörd ist Telefon
 admin.site.register(League)
 admin.site.register(Club)
 admin.site.register(Player)
